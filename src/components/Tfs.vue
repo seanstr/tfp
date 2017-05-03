f8be3eec8e6938669edf754fbbbd0b2d26942571
Sean Matthews <sematthews@PTW.local>
2017-05-03 17:40:28 -0600

    first commit

---
+++ src/components/Tfs.vue
@@ -0,0 +1,40 @@
+<template>
+	<div>
+    <button class="primary full-width">
+      Start New Transaction
+    </button>
+    <div id="app">
+      message
+    </div>
+
+    <div id="app-2">
+      <span>
+        Hover your mouse over me for a few seconds
+        to see my dynamically bound title!
+      </span>
+    </div>
+
+    <div id="app-3">
+      <p>June 13 2017</p>
+    </div>  
+
+    <div id="app-4">
+      <ol>
+        <li>
+          
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
